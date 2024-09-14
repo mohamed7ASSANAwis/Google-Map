343afaa0dac75b186243df8c38b75bdfef95b2bf
MOHAMED7ASSA6 <mh5093351@gmail.com>
2024-09-14 17:54:29 +0300

    3. Google Maps Camera

--- lib/widget/custom_goole_map.dart
+++ lib/widget/custom_goole_map.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 class CustomGooleMap extends StatefulWidget {
   const CustomGooleMap({super.key});
 
@@ -12,11 +11,13 @@ class _CustomGooleMapState extends State<CustomGooleMap> {
   //late CameraTargetBounds cameraTargetBounds;
   late CameraPosition initialCameraPosition;
   late GoogleMapController googleMapController;
+  Set<Marker> marker = {};
 
   @override
   void initState() {
     initialCameraPosition = CameraPosition(
         zoom: 10, target: LatLng(29.373088961975537, 31.172350072218592));
+    initiMarker();
     super.initState();
   }
 
@@ -28,26 +29,60 @@ class _CustomGooleMapState extends State<CustomGooleMap> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        GoogleMap(
-
-            // cameraTargetBounds:cameraTargetBounds,
-            onMapCreated: (controller) => googleMapController = controller,
-            initialCameraPosition: initialCameraPosition),
-        Positioned(
-            bottom: 16,
-            right: 10,
-            left: 10,
-            child: ElevatedButton(
-                onPressed: () {
-                  googleMapController.animateCamera(CameraUpdate.newLatLng(
-                      LatLng(27.177268416767657, 31.18652980376458)));
-                },
-                child: Text("change"))),
-      ],
-    );
+    return GoogleMap(
+        markers: marker,
+        // mapType: MapType.terrain,
+        // cameraTargetBounds:cameraTargetBounds,
+        zoomControlsEnabled: false,
+        onMapCreated: (controller) {
+          googleMapController = controller;
+          intiMapStyle();
+        },
+        initialCameraPosition: initialCameraPosition);
   }
+
+//onMapCreated مينفعش استدعي دي في inst علشان هضرب لازم في
+  void intiMapStyle() async {
+    var intiStyle = await DefaultAssetBundle.of(context)
+        .loadString("assets/map_style/stander.json");
+    googleMapController.setMapStyle(intiStyle);
+  }
+
+  void initiMarker() async {
+    var BitIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(), "assets/images/icon_location1.png");
+    var myMarker = Marker(
+        icon: BitIcon,
+        markerId: MarkerId("1"),
+        position: LatLng(29.349076099818955, 31.209870474283296));
+    marker.add(myMarker);
+    setState(() {});
+  }
+
+//و هنا نقدر تتحكم في حكم marker *******
+//   void initiMarker()async {
+//     var BitIcon=  BitmapDescriptor.fromBytes(await getImageFromRawData("assets/images/icon_location1.png", 100));
+//     var myMarker =  Marker(
+//         icon: BitIcon,
+//         markerId: MarkerId("1"),
+//         position: LatLng(29.349076099818955, 31.209870474283296));
+//     marker.add(myMarker);
+//     setState(() {
+//
+//     });
+//   }
+// Future<Uint8List>getImageFromRawData(String image,double widget)async{
+//   var imageData=await rootBundle.load(image);
+//   var imageCode=await ui.instantiateImageCodec(
+//     imageData.buffer.asUint8List(),
+//     targetWidth: widget.round(),
+//
+//   );
+//   var imageFrame=await imageCode.getNextFrame();
+//   var imageByteData=await imageFrame.image.toByteData(format: ui.ImageByteFormat.png);
+//   return imageByteData!.buffer.asUint8List();
+// }
+//**********
 }
 //eeeeeeeeee
 //1-zoom
@@ -60,3 +95,14 @@ class _CustomGooleMapState extends State<CustomGooleMap> {
 //  cameraTargetBounds=CameraTargetBounds(LatLngBounds(
 //            southwest:LatLng(29.32994143275711, 31.193952961968296) ,northeast: LatLng(29.361670311105442, 31.217856832698235),
 //          ));
+//3.
+//       Positioned(
+//             bottom: 16,
+//             right: 10,
+//             left: 10,
+//             child: ElevatedButton(
+//                 onPressed: () {
+//                   googleMapController.animateCamera(CameraUpdate.newLatLng(
+//                       LatLng(27.177268416767657, 31.18652980376458)));
+//                 },
+//                 child: Text("change"))),
